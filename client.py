a4a1b1584930262574fea16b8bca8a9da4327323
Dex568 <dex0012@protonmail.ch>
2020-07-04 06:41:49 +0100

    made a better cli and tidied up code

--- client.py
+++ client.py
@@ -1,19 +1,10 @@
-import socket, argparse, select, sys, random, string, datetime
+import socket
+import select
+import sys
+import random
+import string
+import datetime
 
-
-'''
-parser = argparse.ArgumentParser(description = 'TCP Client')
-
-parser.add_argument('ip', type=str,
-                    help='The IP to connect to.')
-
-parser.add_argument('port', type=int,
-                    help='The port to use.')
-
-parser.add_argument('-u', '--username', type=str,
-                    help='The username to connect under (if none is given, one will be randomly generated')
-args = parser.parse_args()
-'''
 
 def usernamegen():
     # quick little username generator
@@ -22,8 +13,6 @@ def usernamegen():
 
 
 def main():
-
-    command_list = ['help','about','connect','exit']
 
     # get the current date and time and format it all nice and shit
     dt = datetime.datetime.now()
@@ -40,8 +29,8 @@ def main():
         # probably should whack this in a command handler
         # or make a command list and iterate through
         if menuinput == 'help':
-            x = comm_help()
-            print(x)
+            help = comm_help()
+            print(help)
         elif menuinput == 'connect':
             conn_handle()
         elif menuinput == 'exit':
@@ -52,12 +41,15 @@ def main():
             print('Unknown command!\nType help for a command list.\n')
             continue
 
+
 def comm_help():
 
     help_list = {}
     help_list['help'] = 'Displays this help message.'
     help_list['about'] = 'Information about this program and its creator.'
-    help_list['connect'] = 'Initialise a connection to a chat room (IP Based for now).'
+    help_list['connect'] = '''Initialise a connection to a chat room
+                            (IP Based for now).'''
+
     help_list['exit'] = 'Exits the program.'
 
     # make a nice little top part to the help bar
@@ -71,14 +63,18 @@ def comm_help():
 
     return top.rstrip('\n')
 
+
 def conn_handle():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     ip = input('Enter IP:')
-    port = input ('Enter Port: ')
+    port = input('Enter Port: ')
     port = int(port)
-    username = input('Enter username to connect under:\n(If none is given, one will be randomly generated.)')
 
+    username = input('Enter username to connect under:')
+
+    if username is None:
+        username = usernamegen()
 
     try:
         print('Connecting to {}:{}'.format(ip, port))
@@ -91,10 +87,10 @@ def conn_handle():
         # which fucks things up after a while use select to make things fancy
         inputs = [sys.stdin, s]
 
-        read_sock, write_sock, error_sock = select.select(inputs,[],[])
+        read_sock, write_sock, error_sock = select.select(inputs, [], [])
         # go through each possible sock and if a message is not
         # being broadcasted from server, send a message
-        # select doesn't like python native input and instead loves file objects
+        # select doesn't like python input and instead loves file objects
         # so sys.stdin it is
         for sock in read_sock:
             if sock == s:
@@ -106,12 +102,20 @@ def conn_handle():
                 message = username+':'+message
                 s.send(message.encode(encoding='utf-8'))
 
-                # replay the message back because it won't get broadcasted to the sender
+                # replay the message back
+                # because it won't get broadcasted to the sender
                 print(message)
+
+
+def about():
+    print('''This program was made by Dex\nAll Rights Reserved and all that shit\n
+    Find me on Discord: Dex#0002 or on twitter: @Dex568''')
+    return
+
 
 if __name__ == '__main__':
     try:
         main()
-    except KeyboardInterrupt as e:
-        print(e)
+    except KeyboardInterrupt:
+        print('\nKeyboard Interrupt Received, Exiting!\n')
         sys.exit(1)
--- server.py
+++ server.py
@@ -1,20 +1,59 @@
-import socket, argparse, threading
+import socket
+import threading
+import sys
+import datetime
 
-# stick this in a function or some shit?
-parser = argparse.ArgumentParser(description = 'TCP Server')
 
-parser.add_argument('ip', type=str,
-                    help='The IP to bind (usually localhost or your local IP address).')
+def main():
 
-parser.add_argument('port', type=int,
-                    help='The port to use.')
-args = parser.parse_args()
+    dt = datetime.datetime.now()
+    x = dt.strftime('%H:%M:%S')
+
+    print('------------------')
+    print('Dex\'s Chat Server')
+    print('Current Time: {}'.format(x))
+    print('Enter a command below or type help for a command list')
+
+    while True:
+        menuinput = input('\n> ')
+
+        if menuinput == 'help':
+            help = comm_help()
+            print(help)
+        elif menuinput == 'new':
+            serv_handle()
+        elif menuinput == 'exit':
+            exit()
+        elif menuinput == 'about':
+            about()
+        else:
+            print('Unknown command!\nType help for a command list.\n')
+            continue
+
+
+def comm_help():
+
+    help_list = {}
+    help_list['help'] = 'Displays this help message.'
+    help_list['about'] = 'Information about this program and its creator.'
+    help_list['connect'] = '''Host a chat room for multiple users to connect
+                            to.'''
+    help_list['exit'] = 'Exits the program.'
+
+    # make a nice little top part to the help bar
+    top = '\n Command ' + ' - '
+    top += ' Description\n %s\n' % ('-' * 50)
+
+    # do some fucking witchcraft to make it look good
+    for x in sorted(help_list):
+        dec = help_list[x]
+        top += ' ' + x + ' - ' + dec + '\n'
+
+    return top.rstrip('\n')
 
 
 def handle_conn(conn, clientlist):
-    not_fucked_to_all_shit = True # fuck sake
-    # add an interface like the client, give things like clients connected and shit?
-    # probably add a sleep for the receive so i dont receive every tick or some dumb shit
+    not_fucked_to_all_shit = True  # fuck sake
     # gets the connection and then spits it back out for everyone
     while not_fucked_to_all_shit:
         res = conn.recv(1024)
@@ -23,36 +62,49 @@ def handle_conn(conn, clientlist):
         if not res.decode():
             print('Client returned empty string, breaking thread')
             remove(conn, clientlist)
-            not_fucked_to_all_shit = False # oh fuck
+            not_fucked_to_all_shit = False  # oh fuck
         else:
             messagebroadcast(res, conn, clientlist)
 
-def main(ip, port):
+
+def serv_handle():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    ip = input('Enter an IP to host your chat room on. (Leave blank for localhost): ')
+    if ip is None:
+        ip = '127.0.0.1'
+        print(ip)
+
+    port = input('Enter port: ')
+    port = int(port)
+
     # stops that shitty 'address already in use' shit while testing
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    s.bind((ip, port))
-    s.listen(3) # probably going to change this
+    try:
+        s.bind((ip, port))
+    except Exception as e:
+        print(e)
+
+    s.listen(3)  # probably going to change this
     clientlist = []
 
-    print('listening on {}:{}\n'.format(ip,port))
+    print('listening on {}:{}\n'.format(ip, port))
 
     while True:
         conn, addr = s.accept()
         clientlist.append(conn)
         print('Connection from {}:{}\n'.format(addr[0], addr[1]))
-        # unthreaded broadcast when a new connection occurs
-
 
         # make it fancy and threaded,
         # then people can simultaneously connect
         c_handle = threading.Thread(
-            target = handle_conn,
-            args = (conn,clientlist,)
+            target=handle_conn,
+            args=(conn, clientlist,)
         )
         # have a sort of p2p model or server? fuck knows
         c_handle.start()
+
 
 def messagebroadcast(message, connection, clientlist):
     # my dumb ass thought it would just magically send it to everyone so here
@@ -62,13 +114,26 @@ def messagebroadcast(message, connection, clientlist):
         if clients != connection:
             try:
                 clients.send(message)
-            except:
+            except socket.error:
                 clients.close()
                 # if no answer, remove the client from list
-                remove(clients,clientlist)
+                remove(clients, clientlist)
+
 
 def remove(connection, clientlist):
     if connection in clientlist:
         clientlist.remove(connection)
 
-main(args.ip, args.port)
+
+def about():
+    print('''This program was made by Dex\nAll Rights Reserved and all that shit\n
+    Find me on Discord: Dex#0002 or on twitter: @Dex568''')
+    return
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nKeyboard Interrupt Received, Exiting!\n')
+        sys.exit(1)
